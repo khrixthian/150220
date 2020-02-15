2e0726e24da610a41f2942592a352df20d27c1bc
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-02-15 22:14:01 +0100

    empaketar subida

---
+++ src/main/java/git150220/fdhs.java
@@ -0,0 +1,21 @@
+package git150220;
+
+public class fdhs {
+
+public int triple(int x) {
+		
+		int triple = x*x*x;
+		
+		return triple;
+
+	}
+	
+public int doble(int x) {
+		
+		int doble = x*x;
+		
+		return doble;
+
+	}
+
+}
